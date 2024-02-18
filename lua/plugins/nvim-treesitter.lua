dac9c06b3dcddd9245e723f522154c2af656e5db
Andrei Motinga <andrew.motinga@gmail.com>
2024-02-18 11:11:40 -0800

    this causes wrong indentation in rspec
    it '' do
    expect(1).to eq 1
    end
    
    ie shifts left by itself

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -7,7 +7,6 @@ return {
       require("nvim-treesitter.configs").setup({
         auto_install = true,
         highlight = { enable = true },
-        indent = { enable = true },
         endwise = { enable = true },
       })
     end,
