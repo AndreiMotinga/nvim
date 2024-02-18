f5d09bbed1e67ed6b59da0a3a574ed357dfd0e85
Andrei Motinga <andrew.motinga@gmail.com>
2024-02-18 12:46:42 -0800

    mess with nvim-tree

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -3,15 +3,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("nvim-tree").setup({
-      update_cwd = true,
-      update_focused_file = {
-        enable = true,
-        update_cwd = true,
-        ignore_list = {},
-      },
       filters = {
         git_ignored = false,
-        dotfiles = false,
       },
     })
   end,
