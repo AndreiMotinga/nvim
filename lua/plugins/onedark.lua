2b84a6beca39a5d1890b65d2abef421edc7709f1
Andrei Motinga <andrew.motinga@gmail.com>
2025-05-16 11:12:57 -0700

    use yellow highlight for visual selection

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -5,6 +5,7 @@ return {
       style = "cool",
       highlights = {
         ["@comment"] = { fg = "#969ea6" },
+        Visual = { bg = "#FFFF99", fg = "#000000" },
       },
     })
     require("onedark").load() -- Load AFTER setup
