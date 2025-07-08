c4c138b469b7bff0dd510d2097204da94f6153c6
Andrei Motinga <andrew.motinga@gmail.com>
2025-07-08 12:26:34 -0700

    use ai

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,18 @@
+return {
+  "Exafunction/windsurf.vim",
+  event = "BufEnter",
+}
+
+-- return {
+--   "jackMort/ChatGPT.nvim",
+--   event = "VeryLazy",
+--   config = function()
+--     require("chatgpt").setup()
+--   end,
+--   dependencies = {
+--     "MunifTanjim/nui.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "folke/trouble.nvim", -- optional
+--     "nvim-telescope/telescope.nvim",
+--   },
+-- }
